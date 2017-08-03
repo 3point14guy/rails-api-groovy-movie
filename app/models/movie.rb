94ad0f0676f3e2a9cef70d864a573fe9d6f524d8
3poitn14guy <sunshine.farm.nc@gmail.com>
2017-08-03 08:37:45 -0400

    added validation to movie model
    -had fields be required and specified data types

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
   belongs_to :user
+  validates :name, :year, presence: true
+  validates :year, :rating, numericality: true
+  validates :year, length: { minimum: 4, maximum: 4 }
+  validates :rating, numericality: { less_than_or_equal_to: 5.0, greater_than_or_equal_to: 0 }
 end
